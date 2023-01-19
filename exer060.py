2c8155c2881094937e637a7ec471840d938cf17c
GuilhermeJT <guilhermej.2309@gmail.com>
2023-01-19 10:59:46 -0400

    modificação do código

--- exer060.py
+++ exer060.py
@@ -2,17 +2,7 @@
 
 num = int(input('Digite um número para ver sua tabuada:'))
 
-print('_' * 12)
-print(f'{num} x  1 = {num*1}')
-print(f'{num} x  2 = {num*2}')
-print(f'{num} x  3 = {num*3}')
-print(f'{num} x  4 = {num*4}')
-print(f'{num} x  5 = {num*5}')
-print(f'{num} x  6 = {num*6}')
-print(f'{num} x  7 = {num*7}')
-print(f'{num} x  8 = {num*8}')
-print(f'{num} x  9 = {num*9}')
-print(f'{num} x 10 = {num*10}')
-print('_' * 12)
+for i in range (1, 11):
+    print(f'{num} x {i} ={num * i}')
 
 
