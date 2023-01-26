b6bf38f42f8bfffc613a5def77cd31c09b79fa62
GuilhermeJT <guilhermej.2309@gmail.com>
2023-01-26 11:25:04 -0400

    arquivo modificado

--- exer071.py
+++ exer071.py
@@ -1,3 +1,7 @@
+'''Faça um programa que peça um numero inteiro positivo e em seguida mostre este numero invertido.
+Exemplo:  12376489
+=> 98467321'''
+
 num = int(input('Número: '))
 
 num = str (num)
